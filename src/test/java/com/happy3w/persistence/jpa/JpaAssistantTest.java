1d052b3b6d542b8185aa7585819f8305855a2317
Yushan Gao <ysgao@thoughtworks.com>
2021-01-02 10:41:40 +0800

    chore: fix test

--- src/test/java/com/happy3w/persistence/jpa/JpaAssistantTest.java
+++ src/test/java/com/happy3w/persistence/jpa/JpaAssistantTest.java
@@ -19,18 +19,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import javax.transaction.Transactional;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Rollback
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestApplication.class)
 @DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
